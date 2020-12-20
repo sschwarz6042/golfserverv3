74f9f872b31ba8a33f7772102323e016c8e52854
sschwarz6042 <71792259+sschwarz6042@users.noreply.github.com>
2020-12-20 17:41:17 -0500

    Minor change

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    arg = request.args['arg1']
-    return 'Hello World!'
+    year = int(request.args['year'])
+    return year
